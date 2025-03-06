985e1b342b7ea8f48d0e2d0ab79c60d6570a0df1
Nguyen Dang Tu Luc <nguyendangtuluc2003@gmail.com>
2025-03-06 11:27:24 +0700

    [add] Lab-02

---
+++ Lab-02/cipher/vigenere/__init__.py
@@ -0,0 +1,19 @@
+
+from vigenere_cipher import VigenereCipher
+vigenere_cipher = VigenereCipher()
+
+@app.route("/api/vigenere/encrypt", methods=["POST"])
+def encrypt():
+    data = request.json
+    plain_text = data["plain_text"]
+    key = data["key"]
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({"encrypted_text": encrypted_text})
+
+@app.route("/api/vigenere/decrypt", methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data["cipher_text"]
+    key = data["key"]
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({"decrypted_text": decrypted_text})
